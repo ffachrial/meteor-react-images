009df1bb59ca31dbd4d422b4d0f22d6480215f9a
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-01-21 15:05:45 +0700

    First Commit. Simple React App

---
+++ client/main.jsx
@@ -0,0 +1,17 @@
+// Import React library
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a component
+const App = () => {
+    return (
+        <div>
+            React App #2
+        </div>
+    );
+};
+
+// Render this component to the screen
+Meteor.startup(() => {
+    ReactDOM.render(<App />, document.querySelector('.container'));
+});
