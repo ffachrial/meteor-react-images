77b57b0f51165f973f8ec9e79e829f3b2f4c3e41
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-01-22 22:09:01 +0700

    Create dummy data

--- client/components/image_list.jsx
+++ client/components/image_list.jsx
@@ -3,11 +3,23 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
+const IMAGES = [
+    { tittle: 'Pen', link: 'http://dummyimage.com/600x400' },
+    { tittle: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
+    { tittle: 'Mug', link: 'http://dummyimage.com/600x400' }
+];
+
 // Create component
 const ImageList = () => {
+    const RenderedImages = IMAGES.map(function() {
+        return (
+            <ImageDetail />
+        );
+    });
+
     return (
         <ul>
-            <ImageDetail />
+            {RenderedImages}
         </ul>
     );
 };
